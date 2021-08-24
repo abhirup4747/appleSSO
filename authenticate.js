7dabb4d8cf02c647a0ea86288a652ae99b6d52b6
abhirup4747 <44662245+abhirup4747@users.noreply.github.com>
2021-08-24 19:41:53 +0530

    Update authenticate.js

--- authenticate.js
+++ authenticate.js
@@ -3,7 +3,7 @@ const signInApple = () => {
            window.AppleID.auth.init({
                 clientId : 'com.dunkindonuts.signin',
                 scope : 'name email',
-                redirectURI : 'https://dev2.dunkindonuts.com/bin/servlet/applesigninpoc',
+                redirectURI : 'https://dev2.dunkindonuts.com/bin/servlet/appleRedirect',
                // state : 'openssl_random_pseudo_bytes',               
                 //usePopup : true //or false defaults to false
             });
